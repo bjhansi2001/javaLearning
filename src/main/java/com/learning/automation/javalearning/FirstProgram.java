bd58d8b79ecd5ea74055ab23e8629140951ec45a
bjhansi2001 <115467604+bjhansi2001@users.noreply.github.com>
2022-10-19 19:09:09 -0400

    Initial Commit

---
+++ src/main/java/com/learning/automation/javalearning/FirstProgram.java
@@ -0,0 +1,13 @@
+package com.learning.automation.javalearning;
+
+public class FirstProgram { public static void main(String[] args) {
+		
+	System.out.println("NAME: Jhansi"); 
+	System.out.println("ADDRESS 1: 123"); 
+	System.out.println("ADDRESS 2: xyz Street"); 
+	System.out.println("CITY: Washington"); 
+	System.out.println("STATE: DC"); 
+	System.out.println("ZIP: 12345");
+	
+	}
+}
