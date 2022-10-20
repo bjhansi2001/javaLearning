78a46ab08b3aa3267b0aeaa29cff7f0e329c6e86
bjhansi2001 <115467604+bjhansi2001@users.noreply.github.com>
2022-10-20 18:35:58 -0400

    Create testprogram.java

---
+++ src/main/java/com/learning/automation/javalearning/testprogram.java
@@ -0,0 +1,11 @@
+package com.learning.automation.javalearning;
+
+public class testprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.println("NAME: Jhansi");
+	}
+
+}
